c63d81170283f191453c8c298983c500255a0423
atifnaufal <atifnaufalh@gmail.com>
2026-09-13 19:27:27 +0700

    feat(seed): pastikan adminpusat@pusat.com selalu ter-seed via DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,22 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Admin Pusat (super admin lintas sekolah). Duplikat logika migrasi
+        // ensure_admin_pusat agar akun ini tetap tercipta walau migrasi
+        // tersebut belum/pernah gagal jalan di database Railway.
+        User::updateOrCreate(
+            ['email' => 'adminpusat@pusat.com'],
+            [
+                'name' => 'Admin Pusat',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'aktif' => true,
+                'school_id' => null,
+                'nik' => 'ADM001',
+                'no_hp' => '0811111111',
+            ]
+        );
+
         User::updateOrCreate(
             ['email' => 'admin@sekolah.com'],
             [
